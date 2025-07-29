b65e7810939823ee3fdc5e22e2790c7d30baf1dc
blakej23 <blakejones1991@gmail.com>
2025-07-29 03:49:47 +0000

    final product

--- dice.rb
+++ dice.rb
@@ -33,21 +33,31 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @die = rand(1..20)
   
-  @outcome = "You rolled a #{die}."
+  @outcome = "You rolled a #{@die}."
   
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-  
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
   
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
